66728b851352d97475825d8323159e1ff1b86507
AgusCuevas <cuevasagustin168@gmail.com>
2022-06-06 21:14:33 -0300

    Terminamos registro personal

--- src/Components/RegistroPostulante.jsx
+++ src/Components/RegistroPostulante.jsx
@@ -1,70 +1,104 @@
-import { Box, TextField, Button } from '@mui/material'
-import React, { Fragment } from 'react'
+
+import * as React from 'react';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
 import Header from './Header'
-import '../App.css';
-import { width } from '@mui/system';
+import Button from '@mui/material/Button'
+import { Box, Step, Stepper } from '@mui/material';
 
 
-export default function RegistroPostulante() {
+export default function AddressForm() {
   return (
-      <Fragment>
-          <Header/>
-          
-          <Box sx={{display:"flex", flexDirection:"column", justifyContent:"center", margin:"1rem" , maxWidth:"400px"}}>
-            <TextField className='campos'
-                variant="outlined"
-                label="Nombre"
-                required 
-                sx={{margin:"8px"}}
-            />
-            <TextField
-            className='campo'
-                variant='outlined'
-                label="Apellido"
-                required
-                sx={{margin:"8px"}}
-                />
-            <TextField
-            className='campo'
-                variant='outlined'
-                label="email"
-                required
-                sx={{margin:"8px"}}/>
-            <TextField
-            className='campo'
-                variant='outlined'
-                label="DNI"
-                required
-                sx={{margin:"8px"}}/>
-            <TextField
-            className='campo'
-                variant='outlined'
-                label="Fecha Nacimiento"
-                required
-                sx={{margin:"8px"}}
-                />
-            <TextField
-            className='campo'
-                variant='outlined'
-                label="Nacionalidad"
-                required
-                sx={{margin:"8px"}}/>    
-            <TextField
-            className='campo'
-                variant='outlined'
-                label="Provincia"
-                required
-                sx={{margin:"8px"}}
-                />
-            <TextField
-            className='campo'
-                variant='outlined'
-                label="Localidad"
-                required
-                sx={{margin:"8px"}}/>
-            <Button variant='contained' sx={{margin:"8px"}}>Siguiente paso</Button>
-          </Box>
-              
-      </Fragment>
-  )
+    <React.Fragment>
+    <Header/>
+      <Typography variant="h6" gutterBottom sx={{display:"flex", justifyContent:"center", padding:"1rem"}}>
+        Datos personales
+      </Typography>
+      <Grid container spacing={3} sx={{padding:"2rem", paddingTop:'0'}}>
+        <Grid item xs={6} sm={6}>
+          <TextField
+            required
+            id="nombre"
+            name="Nombre"
+            label="Nombre"
+            fullWidth
+            autoComplete="given-name"
+            variant="standard"
+          />
+        </Grid>
+        <Grid item xs={6} sm={6}>
+          <TextField
+            required
+            id="apellido"
+            name="Apellido"
+            label="Apellido"
+            fullWidth
+            autoComplete="family-name"
+            variant="standard"
+          />
+        </Grid>
+        <Grid item xs={6}>
+          <TextField
+            required
+            id="dni"
+            name="dni"
+            label="DNI"
+            fullWidth
+            autoComplete="shipping address-line1"
+            variant="standard"
+          />
+        </Grid>
+        <Grid item xs={6}>
+          <TextField
+            id="fechaNacimiento"
+            name="fechaNacimiento"
+            label="Fecha de nacimiento"
+            fullWidth
+            autoComplete="shipping address-line2"
+            variant="standard"
+            type='date'
+          />
+        </Grid>
+        <Grid item xs={6} sm={6}>
+          <TextField
+            required
+            id="nacionalidad"
+            name="nacionalidad"
+            label="Nacionalidad"
+            fullWidth
+            autoComplete="shipping address-level2"
+            variant="standard"
+          />
+        </Grid>
+        <Grid item xs={6} sm={6}>
+          <TextField
+            id="provincia"
+            name="provincia"
+            label="Provincia"
+            fullWidth
+            variant="standard"
+          />
+        </Grid>
+        <Grid item xs={6} sm={6}>
+          <TextField
+            required
+            id="localidad"
+            name="localidad"
+            label="Localidad"
+            fullWidth
+            autoComplete="shipping postal-code"
+            variant="standard"
+          />
+        </Grid>
+        <Grid item xs={12}>
+            <Box sx={{display:"flex", justifyContent:"center"}}>
+            <Button variant='contained' color='secondary' >Siguiente</Button>
+            </Box>
+        </Grid>
+      </Grid>
+    </React.Fragment>
+  );
 }
