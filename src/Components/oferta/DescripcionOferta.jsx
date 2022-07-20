5b87c5c4d636540c3d75de48d39718093598c4e8
Joel Yturrieta <joelyturrieta@gmail.com>
2022-07-20 02:51:55 -0300

    Update DescripcionOferta.jsx

--- src/Components/oferta/DescripcionOferta.jsx
+++ src/Components/oferta/DescripcionOferta.jsx
@@ -10,10 +10,10 @@ import DialogTitle from "@mui/material/DialogTitle";
 import PropTypes from "prop-types";
 import ConfirmacionPostulacion from "./ConfirmacionPostulacion";
 import Grid from "@mui/material/Grid";
-import StoreIcon from "@mui/icons-material/Store";
+
 import Header from "../Header";
 import { useState } from "react";
-import Ofertas from "../home/Ofertas";
+
 import { useParams } from "react-router-dom";
 
 const BootstrapDialog = styled(Dialog)(({ theme }) => ({
@@ -75,7 +75,6 @@ const CustomizedDialogs = () => {
     try {
       const api = await fetch(API_URL);
       const datos = await api.json();
-      console.log(datos.Empresa);
       setTituloOferta(datos.titulo_oferta);
       setNombreEmpresa(datos.Empresa.nombre_empresa);
       setDescripcion(datos.descripcion);
@@ -110,27 +109,28 @@ const CustomizedDialogs = () => {
         <Grid
           container
           spacing={3}
-          sx={{ justifyContent: "center", padding: "1rem" }}
+          sx={{ justifyContent: "center", padding:"1rem" }}
         >
-          <Box sx={{ position: "relative" }}>
+          <Box sx={{ display:"flex", flexDirection:"column", margin:"2rem" }}>
             <Box
               sx={{
+                display: "flex",
                 padding: "2rem",
                 justifyContent: "center",
-                display: "flex",
+                
               }}
             >
               <Stack direction="row" spacing={2}>
-                <StoreIcon sx={{ height: "8rem", width: "8rem" }} />
+              <img src="https://cdn.discordapp.com/attachments/955646153297395722/996230598853148792/unknown.png" style={{height:"15rem", width:"15rem"}}/>
               </Stack>
             </Box>
-            <Box sx={{ padding: "2rem" }}>
-              <h1 style={{ display: "flex" }}>{tituloOferta}</h1>
-              <h3 style={{ display: "flex" }}>{nombreEmpresa}</h3>
+            <Box sx={{ display:"flex", justifyContent:"center", flexDirection:"column", maxWidth:"20rem"}}>
+              <Typography variant="h4" sx={{ textAlign:"center"}}>{tituloOferta}</Typography><br />
+              <Typography variant="h6" sx={{textAlign:"justify"}}>{nombreEmpresa}</Typography><br />
             </Box>
             <Box
               sx={{
-                padding: "1rem",
+                
                 display: "flex",
                 justifyContent: "center",
               }}
@@ -140,7 +140,7 @@ const CustomizedDialogs = () => {
                 variant="contained"
                 color="relaxed"
                 onClick={handleClickOpen}
-                sx={{ width: "300px" }}
+                sx={{ width: "20rem" }}
               >
                 Postularme
               </Button>
@@ -156,74 +156,67 @@ const CustomizedDialogs = () => {
             </Box>
           </Box>
 
-          <Box sx={{ padding: "1rem", maxWidth: "30rem" }}>
+          <Box sx={{  maxWidth: "30rem" }}>
             <Box sx={{ borderBottom: "#009688 2px solid" }}>
               {" "}
               <h3>Descripcion:</h3>{" "}
-              <Typography sx={{ fontSize: "20px" }}> {descripcion}</Typography>
+              <Typography sx={{ fontSize: "20px", marginBottom:"1rem" }}> {descripcion}</Typography>
             </Box>
-            <Box sx={{ display: "flex", borderBottom: "#009688 2px solid" }}>
+            <Box sx={{ display: "flex", alignItems:"center", borderBottom: "#009688 2px solid" }}>
               {" "}
-              <h3>Zona de trabajo:</h3>{" "}
-              <Typography sx={{ padding: "1rem", fontSize: "20px" }}>
+              <h3>Zona de trabajo: </h3>{" "}
+              <Typography sx={{ fontSize: "20px", paddingLeft:"0.5rem"}} variant="body1">
                 {zona}
               </Typography>
             </Box>
-            <Box sx={{ display: "flex", borderBottom: "#009688 2px solid" }}>
+            <Box sx={{ display: "flex", alignItems:"center", borderBottom: "#009688 2px solid" }}>
               {" "}
-              <h3>Rango edad:</h3>{" "}
-              <Typography sx={{ padding: "1rem", fontSize: "20px" }}>
+              <h3>Edad:</h3>{" "}
+              <Typography sx={{ paddingLeft:"0.5rem", fontSize: "20px" }}>
                 Desde {edadDesde} hasta {edadHasta} años
               </Typography>
             </Box>
-            <Box sx={{ display: "flex", borderBottom: "#009688 2px solid" }}>
+            <Box sx={{ display: "flex", alignItems:"center", borderBottom: "#009688 2px solid" }}>
               {" "}
               <h3>Horario:</h3>{" "}
-              <Typography sx={{ padding: "1rem", fontSize: "20px" }}>
+              <Typography sx={{ paddingLeft:"0.5rem", fontSize: "20px" }}>
                 De {horarioEnrada} a {horarioSalida}
               </Typography>
             </Box>
-            <Box sx={{ display: "flex", borderBottom: "#009688 2px solid" }}>
+            <Box sx={{ display: "flex", alignItems:"center", borderBottom: "#009688 2px solid" }}>
               {" "}
               <h3>Idiomas:</h3>{" "}
               <Typography
-                sx={{ padding: "1rem", fontSize: "20px" }}
+                sx={{ paddingLeft:"0.5rem", fontSize: "20px" }}
               ></Typography>
             </Box>
-            <Box sx={{ display: "flex", borderBottom: "#009688 2px solid" }}>
+            <Box sx={{ display: "flex", alignItems:"center", borderBottom: "#009688 2px solid" }}>
               {" "}
               <h3>Jornada: </h3>{" "}
-              <Typography sx={{ padding: "1rem", fontSize: "20px" }}>
+              <Typography sx={{ paddingLeft:"0.5rem", fontSize: "20px" }}>
                 {jornada}
               </Typography>
             </Box>
-            <Box sx={{ display: "flex", borderBottom: "#009688 2px solid" }}>
+            <Box sx={{ display: "flex", alignItems:"center", borderBottom: "#009688 2px solid" }}>
               {" "}
               <h3>Contrato: </h3>{" "}
-              <Typography sx={{ padding: "1rem", fontSize: "20px" }}>
+              <Typography sx={{ paddingLeft:"0.5rem", fontSize: "20px" }}>
                 {contrato}
               </Typography>
             </Box>
-            <Box sx={{ display: "flex", borderBottom: "#009688 2px solid" }}>
+            <Box sx={{ display: "flex", alignItems:"center", borderBottom: "#009688 2px solid" }}>
               {" "}
               <h3>Beneficios: </h3>{" "}
-              <Typography sx={{ padding: "1rem", fontSize: "20px" }}>
+              <Typography sx={{ paddingLeft:"0.5rem", fontSize: "20px" }}>
                 {beneficios}
               </Typography>
             </Box>
-            <Box sx={{ display: "flex", borderBottom: "#009688 2px solid" }}>
+            <Box sx={{ display: "flex", alignItems:"center", borderBottom: "#009688 2px solid" }}>
               {" "}
               <h3>Salario:</h3>{" "}
-              <Typography sx={{ padding: "1rem", fontSize: "20px" }}>
+              <Typography sx={{ paddingLeft:"0.5rem", fontSize: "20px" }}>
                 ${salario}
               </Typography>
-            </Box>
-            <Box sx={{ display: "flex" }}>
-              {" "}
-              <h3>Nombre del representante:</h3>{" "}
-              <Typography
-                sx={{ padding: "1rem", fontSize: "20px" }}
-              ></Typography>
             </Box>
           </Box>
         </Grid>
