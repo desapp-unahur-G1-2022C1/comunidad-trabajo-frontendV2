ab7f32a4a00a5587a0a749aa7e5b0325da2d1bbe
Joel Yturrieta <joelyturrieta@gmail.com>
2022-08-03 02:01:13 -0300

    no funciona

--- src/Components/home/Home.jsx
+++ src/Components/home/Home.jsx
@@ -9,15 +9,20 @@ import { useState } from 'react';
 import BarraBusqueda from './BarraBusqueda';
 import { Typography } from '@material-ui/core';
 import BusquedaNoEncontrada from './BusquedaNoEncontrada';
+import { Pagination } from '@mui/material';
 
 
 
 const Home = () => {
 
   const [listaOfertas, setListaOfertas] = useState([]);
-  const [llamado, setLlamado] = useState(false)
+  const [llamado, setLlamado] = useState(false);
+  const [cantPaginas, setCantPaginas] = useState(0);
+  const [pagina, setPagina] = useState(1);
 
-  const API_URL = `https://comunidad-de-trabajo.herokuapp.com/ofertas`;
+  
+  
+  const API_URL = `https://comunidad-de-trabajo.herokuapp.com/ofertas/?pagina=0&limite=3&ordenar=id`;
 
   const primerLlamado = async () => {
     if (llamado === false){
@@ -25,6 +30,7 @@ const Home = () => {
         const api = await fetch(API_URL);
         const datos = await api.json();
         setListaOfertas(datos.ofertas.rows)
+        setCantPaginas(datos.totalPaginas)
         setLlamado(true)
       } catch (error) {
         console.log(error)
@@ -37,14 +43,25 @@ const Home = () => {
       e.preventDefault()
       const {ofertas} = e.target.elements;
       const ofertasValue = ofertas.value;
-      const api = await fetch(`https://comunidad-de-trabajo.herokuapp.com/ofertas/?pagina=0&limite=10&buscarTitulo=${ofertasValue}`);
+      const api = await fetch(`https://comunidad-de-trabajo.herokuapp.com/ofertas/?pagina=${pagina}&limite=3&buscarTitulo=${ofertasValue}&ordenar=id `);
       const datos = await api.json();
       setListaOfertas(datos.ofertas.rows);
+      setCantPaginas(datos.totalPaginas)
       console.log(datos.ofertas.rows)
      
       } catch (err) {
         console.log(err);
     }
+  }
+
+  const cambiarPagina = async (e, p) => {
+    
+    const api = await fetch(`https://comunidad-de-trabajo.herokuapp.com/ofertas/?pagina=${pagina}&limite=3&ordenar=id `);
+    const datos = await api.json();
+    setListaOfertas(datos.ofertas.rows);
+    setCantPaginas(datos.totalPaginas)
+    setPagina(p)
+    console.log(datos.ofertas.rows)
   }
   
   primerLlamado()
@@ -62,7 +79,7 @@ const Home = () => {
           <BusquedaNoEncontrada/>
           : <Ofertas
           listaOfertas={listaOfertas}/> }
-           
+           <Pagination count={cantPaginas} page={pagina} onChange={cambiarPagina} variant='outlined'></Pagination>
           </div>
         </div>
       </Fragment> );
