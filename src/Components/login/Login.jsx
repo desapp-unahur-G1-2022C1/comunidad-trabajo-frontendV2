cde7218f3ee609e9acb7eab026f8c8ddc1895e8e
Joel Yturrieta <joelyturrieta@gmail.com>
2022-08-06 18:33:39 -0300

    Update Login.jsx

--- src/Components/login/Login.jsx
+++ src/Components/login/Login.jsx
@@ -1,9 +1,11 @@
-import { FormControl, TextField } from '@mui/material';
+import { FormControl, TextField, Box, Typography } from '@mui/material';
 import React, { Fragment } from 'react';
 import { useState } from 'react';
 import Header from "../Header"
 import Button from '@mui/material/Button';
 import axios from "axios"
+import { Link} from "react-router-dom"
+
 const Login = () => {
 
     const [body, setBody] = useState({usuario:'', password:''})
@@ -29,28 +31,34 @@ const Login = () => {
     return ( 
         <Fragment>
             <Header/>
-            <form onSubmit="#">
-                <TextField
+            <Typography sx={{display:"flex", justifyContent:"center", margin:"2rem"}} variant='h4'> Iniciar sesión </Typography>
+            <Box sx={{display:"flex", justifyContent:"center", margin:"2rem", }}>
+                <form onSubmit="#">
+                <Box sx={{display:"flex", flexDirection:"column", margin:"1.5rem", width:"20rem" }}><TextField 
                 type="text"
                 value={body.usuario}
                 name="usuario"
                 placeholder='Usuario'
                 onChange={inputChange}
                 >
-                </TextField>
+                </TextField></Box>
                 
-                <TextField
+                <Box sx={{display:"flex", flexDirection:"column", margin:"1.5rem"}}><TextField
                 type="password"
                 value={body.password}
                 name="password"
                 placeholder='Contraseña'
                 onChange={inputChange}
                 >
-                </TextField>
-                <Button onClick={handleSubmit}>
+                </TextField></Box>
+                <Box sx={{display:"flex", justifyContent:"center", margin:"1.5rem"}}><Button fullWidth color='relaxed' onClick={handleSubmit}  variant="contained">
                     Ingresar
-                </Button>
-            </form>
+                </Button></Box>
+            </form></Box>
+            <Box sx={{display:"flex", justifyContent:"center"}}>
+          <Link to="/recuperarContrasena"style={{ textDecoration: 'none'}}><Typography sx={{display:"flex", margin:"0.5rem", justifyContent:"flex-start", color:"#3f50b5"}}>¿Olvidaste tu contraseña?</Typography></Link>
+          <Link to="/registroUsuario"style={{ textDecoration: 'none'}}><Typography sx={{display:"flex", margin:"0.5rem", justifyContent:"flex-end", color:"#3f50b5"}}>Registrate</Typography></Link>
+        </Box>
             
         </Fragment>
      );
