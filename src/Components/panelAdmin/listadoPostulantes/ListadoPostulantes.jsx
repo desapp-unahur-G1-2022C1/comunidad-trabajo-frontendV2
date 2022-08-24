4834c223357f8e95e8301a7c59b08c94516d2724
Joel Yturrieta <joelyturrieta@gmail.com>
2022-08-24 18:06:05 -0300

    paginado de ofertas para admin

--- src/Components/panelAdmin/listadoPostulantes/ListadoPostulantes.jsx
+++ src/Components/panelAdmin/listadoPostulantes/ListadoPostulantes.jsx
@@ -3,7 +3,7 @@ import { TextField } from '@mui/material/TextField';
 import { Button } from '@mui/material/Button';
 import { Box } from '@mui/system';
 import Header from "../../Header"
-import { Grid, Typography } from '@mui/material';
+import { Grid, Pagination, Typography } from '@mui/material';
 import BarraBusquedaPostulantes from './BarraBusquedaPostuluantes';
 import ListaPostulantes from './ListaPostulantes';
 import BusquedaNoEncontrada from './BusquedaNoEncontrada';
@@ -13,17 +13,20 @@ const ListadoPostulantes = () => {
 
     const [llamado, setLlamado] = useState(false);
     const [postulantes, setPostulantes] = useState([]);
+    const [cantPaginas, setCantPaginas] = useState(0);
+    const [pagina, setPagina] = useState(1);
 
-    const API_URL = `https://comunidad-de-trabajo.herokuapp.com/usuariosPostulantes/`
+    const API_URL = `https://comunidad-backend-v3.herokuapp.com/postulantes/?pagina=0&limite=3`;
 
     const primerLlamado = async () => {
         if(llamado === false){
             try{
-            const api = await fetch(API_URL);
-            const datos = await api.json();
-            setLlamado(true)
-            setPostulantes(datos.postulantes.rows)
-            
+                const api = await fetch(API_URL);
+                const datos = await api.json();
+                setLlamado(true)
+                setPostulantes(datos.postulantes.rows)
+                setCantPaginas(datos.totalPaginas)
+                
             }
             catch(error){
                 console.log(error)
@@ -31,22 +34,32 @@ const ListadoPostulantes = () => {
         }
     }
 
-    const traerPostulantes = async (e) => {
+    const traerPostulantes = async (e, p) => {
         try{
             e.preventDefault()
             const {usuario} = e.target.elements;
             const usuarioValue = usuario.value;
-            const api = await fetch(`https://comunidad-de-trabajo.herokuapp.com/usuariosPostulantes/?buscarApellido=${usuarioValue}`);
+            const api = await fetch(`https://comunidad-backend-v3.herokuapp.com/postulantes/?pagina=${p - 1}&limite=3&buscarNombre=${usuarioValue}`);
             const datos = await api.json();
-            console.log(datos.postulantes.rows)
+            console.log(datos)
             setPostulantes(datos.postulantes.rows)
-            
-            
+            setCantPaginas(datos.totalPaginas)
+            console.log(postulantes)
         }
         catch(err){
             console.log(err)
         }
     }
+
+    const cambiarPagina = async (e, p) => {
+        
+        const api = await fetch(`https://comunidad-backend-v3.herokuapp.com/postulantes/?pagina=${p - 1}&limite=3&ordenar=id`);;
+        const datos = await api.json();
+        setPostulantes(datos.postulantes.rows);
+        setPagina(p)
+        console.log(datos.ofertas.rows)
+    }
+
 
     primerLlamado()
     return (  
@@ -57,8 +70,9 @@ const ListadoPostulantes = () => {
             {postulantes.length === 0 && llamado === true ?
             <BusquedaNoEncontrada/> :
             <ListaPostulantes
-            postulantes={postulantes}/>
-            }
+            postulantes={postulantes}/>}
+            <Pagination color="primary" count={cantPaginas} page={pagina} onChange={cambiarPagina} />
+        
             
             
         </Fragment>
