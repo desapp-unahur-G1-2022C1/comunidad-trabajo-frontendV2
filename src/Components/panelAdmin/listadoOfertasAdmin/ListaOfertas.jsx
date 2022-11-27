c96059f00e28ecb0f3c0a7cef729aa7afc596339
Joel Yturrieta <63125197+jyturrieta@users.noreply.github.com>
2022-11-27 01:36:12 -0300

    se pueden mandar ofertas a revisión

--- src/Components/panelAdmin/listadoOfertasAdmin/ListaOfertas.jsx
+++ src/Components/panelAdmin/listadoOfertasAdmin/ListaOfertas.jsx
@@ -8,8 +8,45 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import { Button, Typography } from '@mui/material';
 import { Link } from 'react-router-dom';
+import Swal from 'sweetalert2';
+
 
 export default function ListaOfertas({ofertas}) {
+  const mandarARevision = async (idOferta) => {
+    var data = {
+      idEstado: 4
+    };
+      await fetch(`https://comunidad-backend-v3.herokuapp.com/ofertas/idOferta/${idOferta}`, {
+      method: "PUT", // or 'PUT'
+      body: JSON.stringify(data), // data can be `string` or {object}!
+      headers: {
+        "Content-Type": "application/json",
+      },
+      
+      })
+      Swal.fire({
+        icon: 'success',
+        title: 'La oferta fue aceptada exitosamente',
+        confirmButtonText: 'Finalizar',
+        text: 'Para continuar pulse el boton',
+        footer: '',
+        showCloseButton: true
+      })
+      .then(
+        window.location.reload()
+      )
+      .catch((error) => console.error("Error:", error,
+      Swal.fire({
+        icon: 'error',
+        title: 'Ocurrio un error al aceptar la oferta',
+        confirmButtonText: 'Volver',
+        text: 'Verifique sus datos',
+        footer: '',
+        showCloseButton: true
+      })),)
+  }
+
+
   return (
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
@@ -34,7 +71,7 @@ export default function ListaOfertas({ofertas}) {
               <TableCell align="left"><Typography variant="body1">{oferta.titulo_oferta}</Typography></TableCell>
               <TableCell align="left"><Typography variant="body1"></Typography>{oferta.Empresa.nombre_empresa}</TableCell>
               <TableCell align="left"><Typography variant="body1">{oferta.Estado.nombre_estado}</Typography></TableCell>
-              <TableCell align="left"><Link style={{textDecoration:"none"}} to={`/oferta/${oferta.id}`}><Button variant="contained" color='relaxed'sx={{margin:"0.5rem"}}>VER OFERTA</Button></Link><Button variant="outlined" color='error'sx={{margin:"0.5rem"}}>Accion2</Button></TableCell>
+              <TableCell align="left"><Link style={{textDecoration:"none"}} to={`/oferta/${oferta.id}`}><Button variant="contained" color='relaxed'sx={{margin:"0.5rem"}}>VER OFERTA</Button></Link><Button variant="outlined" color='error'sx={{margin:"0.5rem"}} onClick={async ()=> mandarARevision(oferta.id)} >DESHABILITAR</Button></TableCell>
               
             </TableRow>
           ))} 
--- src/Components/panelAdmin/listadoOfertasAdmin/ListadoOfertas.jsx
+++ src/Components/panelAdmin/listadoOfertasAdmin/ListadoOfertas.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useState} from 'react'
+import React, { Fragment, useState } from 'react'
 
 import Header from "../../Header"
 import { Box, Button, Pagination } from '@mui/material';
@@ -19,24 +19,24 @@ const ListadoOfertas = () => {
     const API_URL = `https://comunidad-backend-v3.herokuapp.com/ofertas/?pagina=0&limite=3&ordenar=id&idEstado=1`;
 
     const primerLlamado = async () => {
-        if(llamado === false){
-            try{
+        if (llamado === false) {
+            try {
                 const api = await fetch(API_URL);
                 const datos = await api.json();
                 setLlamado(true)
                 setOfertas(datos.ofertas.rows)
                 setCantPaginas(datos.totalPaginas)
             }
-            catch(error){
+            catch (error) {
                 console.log(error)
             }
         }
     }
 
     const traerOfertas = async (e, p) => {
-        try{
+        try {
             e.preventDefault()
-            const {oferta} = e.target.elements;
+            const { oferta } = e.target.elements;
             const ofertaValue = oferta.value;
             setBusquedaActual(ofertaValue);
             const api = await fetch(`https://comunidad-backend-v3.herokuapp.com/ofertas/?pagina=0&limite=3&ordenar=id&idEstado=1&buscarTitulo=${ofertaValue}`);
@@ -47,13 +47,13 @@ const ListadoOfertas = () => {
             setCantPaginas(datos.totalPaginas)
             console.log(ofertas)
         }
-        catch(err){
+        catch (err) {
             console.log(err)
         }
     }
 
     const cambiarPagina = async (e, p) => {
-        
+
         const api = await fetch(`https://comunidad-backend-v3.herokuapp.com/ofertas/?pagina=${p - 1}&limite=3&ordenar=id&idEstado=1&buscarTitulo=${busquedaActual}`);
         const datos = await api.json();
         setOfertas(datos.ofertas.rows);
@@ -63,25 +63,34 @@ const ListadoOfertas = () => {
 
 
     primerLlamado()
-    return (  
+    return (
         <Fragment>
-            <Header/>
+            <Header />
             <BarraBusquedaOfertas
-            traerOfertas={traerOfertas}/>
-            <Box sx={{display:"flex", justifyContent:"start"}}>
-                <Link to="/admin/listadoOfertasInactivas" style={{textDecoration:"none"}}>
-                    <Button variant="contained" color='edit' sx={{margin:"0.5rem"}}>
-                        Aceptar ofertas
-                    </Button>
-                </Link>
+                traerOfertas={traerOfertas} />
+            <Box sx={{display:"flex" }}>
+                <Box sx={{ display: "flex", justifyContent: "start" }}>
+                    <Link to="/admin/listadoOfertasInactivas" style={{ textDecoration: "none" }}>
+                        <Button variant="contained" color='edit' sx={{ margin: "0.5rem" }}>
+                            Ofertas pendientes
+                        </Button>
+                    </Link>
+                </Box>
+                <Box sx={{ display: "flex", justifyContent: "start" }}>
+                    <Link to="/admin/listadoOfertasInactivas" style={{ textDecoration: "none" }}>
+                        <Button variant="contained" color='error' sx={{ margin: "0.5rem" }}>
+                            Ofertas en revisión
+                        </Button>
+                    </Link>
+                </Box>
             </Box>
             {ofertas.length === 0 && llamado === true ?
-            <BusquedaNoEncontrada/> :
-            <ListaOfertas
-            ofertas={ofertas}/>}
-            <Pagination color="primary" count={cantPaginas} page={pagina} onChange={cambiarPagina} sx={{display:"flex", justifyContent:"center", margin:"1rem"}}/>
+                <BusquedaNoEncontrada /> :
+                <ListaOfertas
+                    ofertas={ofertas} />}
+            <Pagination color="primary" count={cantPaginas} page={pagina} onChange={cambiarPagina} sx={{ display: "flex", justifyContent: "center", margin: "1rem" }} />
         </Fragment>
     );
 }
- 
+
 export default ListadoOfertas;
