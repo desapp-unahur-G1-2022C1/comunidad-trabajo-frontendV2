6317f64d40faabb80924e8cd61652162d8dfa1e8
Joel Yturrieta <63125197+jyturrieta@users.noreply.github.com>
2022-11-28 01:41:09 -0300

    Update ListaPostulantes.jsx

--- src/Components/oferta/listadoDePostulantes/ListaPostulantes.jsx
+++ src/Components/oferta/listadoDePostulantes/ListaPostulantes.jsx
@@ -13,47 +13,95 @@ import Swal from 'sweetalert2';
 import CheckIcon from '@mui/icons-material/Check';
 import CloseIcon from '@mui/icons-material/Close';
 import PictureAsPdfIcon from '@mui/icons-material/PictureAsPdf';
+import { useState, useEffect } from 'react';
 
-export default function ListaPostulantes({postulantes}) {
-  
+
+
+export default function ListaPostulantes({ postulantes }) {
+
+
+  const [pdf, setPdf] = useState(null);
+
   const contactar = async (idPostulacion) => {
+
+
+
+
+
+
     var data = {
       contactado: true
     };
-      await fetch(`https://comunidad-backend-v3.herokuapp.com/postulaciones/${idPostulacion}`, {
+    await fetch(`https://comunidad-backend-v3.herokuapp.com/postulaciones/${idPostulacion}`, {
       method: "PUT", // or 'PUT'
       body: JSON.stringify(data), // data can be `string` or {object}!
       headers: {
         "Content-Type": "application/json",
       },
-      
-      })
-      Swal.fire({
-        icon: 'success',
-        title: 'El postulante fue contactado',
-        confirmButtonText: 'Finalizar',
-        text: 'Para continuar pulse el boton',
-        footer: '',
-        showCloseButton: true
-      })
+
+    })
+    Swal.fire({
+      icon: 'success',
+      title: 'El postulante fue contactado',
+      confirmButtonText: 'Finalizar',
+      text: 'Para continuar pulse el boton',
+      footer: '',
+      showCloseButton: true
+    })
       .then(
         window.location.reload()
       )
       .catch((error) => console.error("Error:", error,
-      Swal.fire({
-        icon: 'error',
-        title: 'Ocurrio un error al contactar al postulante',
-        confirmButtonText: 'Volver',
-        text: 'Verifique sus datos',
-        footer: '',
-        showCloseButton: true
-      })),)
+        Swal.fire({
+          icon: 'error',
+          title: 'Ocurrio un error al contactar al postulante',
+          confirmButtonText: 'Volver',
+          text: 'Verifique sus datos',
+          footer: '',
+          showCloseButton: true
+        })),)
   }
-  
 
-  
 
-  
+  function splitFileName(str) {
+    return str.split("|")[1];
+  }
+
+
+  const traerPdf = async (cvPostulante) => {
+    const fetchedData = await axios.get(
+      `https://comunidad-backend-v3.herokuapp.com/files`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          "type": "application/pdf",
+          "file": splitFileName(cvPostulante)
+        },
+        responseType: 'blob'
+      }
+    );
+
+    console.log(fetchedData);
+    const pdfBlob = new Blob([fetchedData.data], { type: "application/pdf" });
+    console.log(pdfBlob);
+    const virtualUrl = URL.createObjectURL(pdfBlob);
+    console.log(virtualUrl);
+    setPdf(virtualUrl);
+    
+
+  };
+
+
+
+  function abrirPdf(cvPostulante) {
+    traerPdf(cvPostulante);
+    window.open(pdf);
+  }
+
+
+
+
+
   return (
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
@@ -70,24 +118,24 @@ export default function ListaPostulantes({postulantes}) {
         <TableBody>
           {postulantes.map((postulante) => (
             <TableRow
-              
+
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
               <TableCell component="th" scope="row">
-              {postulante.Postulante.nombre} {postulante.Postulante.apellido}
+                {postulante.Postulante.nombre} {postulante.Postulante.apellido}
               </TableCell>
               <TableCell align="center"><Typography variant="body1">{postulante.Postulante.id}</Typography></TableCell>
               <TableCell align="center"><Typography variant="body1"></Typography>{postulante.Postulante.telefono}</TableCell>
-              <TableCell align="center"><Typography variant="body1"><PictureAsPdfIcon color="error"/></Typography></TableCell>
-              <TableCell align="center"><Typography variant="body1"></Typography>{postulante.contactado? <CheckIcon color="success"/>: <CloseIcon color="error"/> }</TableCell>
+              <TableCell align="center"><Button onClick={async () =>abrirPdf(postulante.Postulante.cv)}><PictureAsPdfIcon color="error" /></Button></TableCell>
+              <TableCell align="center"><Typography variant="body1"></Typography>{postulante.contactado ? <CheckIcon color="success" /> : <CloseIcon color="error" />}</TableCell>
               <TableCell align="center">
-                <Link to={`/postulante/${postulante.Postulante.id}`} style={{textDecoration:'none'}}>
-                  <Button variant="contained" color='relaxed'sx={{margin:"0.5rem"}}>
+                <Link to={`/postulante/${postulante.Postulante.id}`} style={{ textDecoration: 'none' }}>
+                  <Button variant="contained" color='relaxed' sx={{ margin: "0.5rem" }}>
                     Ver
                   </Button>
                 </Link>
-                
-                {postulante.contactado? <Button variant="contained" color='info'sx={{margin:"0.5rem"}} onClick={ async ()=> contactar(postulante.id)} disabled >Contactado</Button> : <Button variant="contained" color='info'sx={{margin:"0.5rem"}} onClick={ async ()=> contactar(postulante.id)}  >Contactado</Button> }
+
+                {postulante.contactado ? <Button variant="contained" color='info' sx={{ margin: "0.5rem" }} onClick={async () => contactar(postulante.id)} disabled >Contactado</Button> : <Button variant="contained" color='info' sx={{ margin: "0.5rem" }} onClick={async () => contactar(postulante.id)}  >Contactado</Button>}
               </TableCell>
             </TableRow>
           ))}
